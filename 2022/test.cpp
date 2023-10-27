1a3b0641f002758b699e0d8100d18f088640a0a8
U7K4R5H <u7@MSI.localdomain>
2023-10-28 01:44:21 +0530

    Day1

---
+++ 2022/test.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+//push https://github.com/U7K4R5H/cses.fi-problemset.git master
+
+int main() {
+	cout << "Game Over!!" << "\n";
+	fstream myFile;
+	string line;
+	ll curr = 0;
+	ll sum = 0;
+	vector<ll> vec;
+	//size_t sz;
+	myFile.open("input.txt", ios::in);
+	if (myFile.is_open()) {
+
+		while(getline(myFile, line)) {
+			if(line == "") {
+				if (sum < curr) {
+					sum = curr;
+				}
+				vec.push_back(curr);
+				curr = 0;
+			}
+			else {
+			curr += (ll)stol(line, nullptr);
+			}
+		}
+		
+		myFile.close();
+	}
+	else {
+		cout << "IO read fail \n";
+	}
+	sort(vec.begin(), vec.end());
+	int n = vec.size();
+	ll three_sum = vec[n - 1] + vec[n - 2] + vec[n -3];
+
+	cout << three_sum << "\n"; 
+	return 0;
+}
